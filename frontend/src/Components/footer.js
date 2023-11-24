a5c37543b2473ac9502e3dd9399684de64549ba1
guibe <Maxipuce>
2023-11-24 21:38:26 +0800

    Files added (images and header footer)

---
+++ frontend/src/Components/footer.js
@@ -0,0 +1,11 @@
+import './footer.css';
+
+export default function Footer() {
+    return (
+        <footer>
+            <p><strong>Compete for the 1st place on the scoreboard !</strong>
+                <br/><italic>Quizzoraurus™</italic></p>            
+        </footer>
+    );
+}
+  
